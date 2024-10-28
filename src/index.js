22fcc884c819b4805769a231a77d677b927cd668
daddyjanno <jn.drugmand@gmail.com>
2024-10-28 10:13:32 +0100

    create map Sprite

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { Application } from 'pixi.js'
+import { Application, Sprite } from 'pixi.js'
 
 console.log('Fortnite Replay Engine')
 
@@ -8,3 +8,11 @@ await app.init({
     antialias: true,
 })
 document.body.appendChild(app.canvas)
+
+const map = Sprite.from('https://assets.codepen.io/39394/fortnite-map.jpg')
+console.log(map)
+
+map.anchor.set(0.5)
+map.x = app.screen.width / 2
+map.y = app.screen.height / 2
+app.stage.addChild(map)
