20124002b6e2e43caba7bdc2106403ecb2f8cc04
daddyjanno <jn.drugmand@gmail.com>
2024-10-28 09:48:57 +0100

    initialize app

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("Fortnite Replay Engine");
+import { Application } from 'pixi.js'
+
+console.log('Fortnite Replay Engine')
+
+const app = new Application()
+await app.init({
+    resizeTo: window,
+    antialias: true,
+})
+document.body.appendChild(app.canvas)
